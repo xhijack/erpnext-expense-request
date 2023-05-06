37382b1f82ec1107a6e83fe0352cac2e0a540747
ramdani <ramdani@192.168.1.4>
2023-05-06 15:27:06 +0700

    add cancelled

--- expense_request/expense_request/doctype/expense_entry/expense_entry.py
+++ expense_request/expense_request/doctype/expense_entry/expense_entry.py
@@ -4,7 +4,15 @@
 
 from __future__ import unicode_literals
 # import frappe
+import frappe
 from frappe.model.document import Document
 
 class ExpenseEntry(Document):
-	pass
+	def before_cancel(self):
+		journal_entry = frappe.db.get_value("Journal Entry", {"bill_no": self.name})
+		if journal_entry:
+			journal_entry_doc = frappe.get_doc("Journal Entry", journal_entry)
+			if journal_entry_doc.docstatus == 1:
+				journal_entry_doc.cancel()
+		self.status = "cancelled"
+		frappe.db.commit()
